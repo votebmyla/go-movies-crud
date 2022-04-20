1494541d1e2963b98b55be32539521da1a4fe0a1
votebmyla <votebmyla@gmail.com>
2022-04-20 21:32:06 +0500

    add getmovies function

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"math/rand"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Movie struct {
@@ -17,6 +21,43 @@ type Director struct {
 	Lastname  string `json:"lastname"`
 }
 
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
 func main() {
-	fmt.Println(rand.Intn(1000))
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{
+		ID:    "1",
+		Isbn:  "654682",
+		Title: "Movie One",
+		Director: &Director{
+			Firstname: "Robert",
+			Lastname:  "Zemeckis",
+		},
+	})
+
+	movies = append(movies, Movie{
+		ID:    "2",
+		Isbn:  "879315",
+		Title: "Movie Two",
+		Director: &Director{
+			Firstname: "Steven",
+			Lastname:  "Spielberg",
+		},
+	})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
 }
